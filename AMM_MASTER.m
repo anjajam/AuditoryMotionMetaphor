1a54a0fefd160a47effefd9d7247a1948aae9e63
anjajam <a.jamrozik@gmail.com>
2016-08-25 15:27:00 -0400

    MASTER - updates

--- AMM_MASTER.m
+++ AMM_MASTER.m
@@ -3,9 +3,18 @@
 %
 % Written by Anja Jamrozik and Andrew S Bock Aug 2016
 
-%% Sort the dicoms and conver to nifti
+
+%% Set up initial variables
+dataDir = '/data/jag/ajamrozik/Data/AMMet/';
+subjDirs = listdir(dataDir,'dirs');
+
+
+%% Sort the dicoms and convert to nifti
 
 %%%% add these scripts %%%%
+
+%% Run Freesurfer's recon-all
+
 
 %% Anything else we did 
 
@@ -18,5 +27,12 @@
 
 %% Register the functional data to the freesurfer anatomical data
 % Use Freesurfer's 'bbregister' function
+
+for i = 1:length(subjDirs)
+    subDir = fullfile(dataDir,subjDirs{i});
+    sessDir = listdir(subDir,'dirs');
+    
+    bbregister(subject_name,filefor_reg,bbreg_out_file,acq_type);
+end
 
 
