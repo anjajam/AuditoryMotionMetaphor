094b7fd9300866f069bcf322986b549b3a70eab8
anjajam <a.jamrozik@gmail.com>
2016-08-25 15:16:24 -0400

    added 'AMM_MASTER' script

---
+++ AMM_MASTER.m
@@ -0,0 +1,22 @@
+%% AMM MASTER SCRIPT
+% This script will process the data for the auditory motion metaphor study
+%
+% Written by Anja Jamrozik and Andrew S Bock Aug 2016
+
+%% Sort the dicoms and conver to nifti
+
+%%%% add these scripts %%%%
+
+%% Anything else we did 
+
+%%% e.g. slice timing correction? %%%
+
+%%% e.g. Joe's scripts %%% this could just be a description, or a link to
+%%% the python scripts
+
+%%% feat stuff %%%
+
+%% Register the functional data to the freesurfer anatomical data
+% Use Freesurfer's 'bbregister' function
+
+
